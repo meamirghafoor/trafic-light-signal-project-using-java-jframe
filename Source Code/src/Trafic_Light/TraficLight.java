158f8589b57bcb6d8a03fe8465f73bec7e7cc381
meamirghafoor <amirghafoor594@gmail.com>
2022-08-27 11:27:02 +0500

    git

---
+++ Source Code/src/Trafic_Light/TraficLight.java
@@ -0,0 +1,23 @@
+package Trafic_Light;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author neutron
+ */
+public class TraficLight {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI obj=new GUI();
+        obj.setVisible(true);
+    }
+    
+}
